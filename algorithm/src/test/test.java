8234d303834a5a15170cf21933edc0fa309f50ea
JiEung2 <https://github.com/JiEung2>
2023-10-14 17:58:08 +0900

    2023.10.14

--- algorithm/src/test/test.java
+++ algorithm/src/test/test.java
@@ -2,29 +2,92 @@ package test;
 import java.util.*;
 public class test {
     public static void main(String[] args) {
-        String[] name = {"may", "kein", "kain", "radi"};
-        int[] yearning = {5, 10, 1, 3};
-        String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+        String[] park = {"SOO","OOO","OOO"};
+        String[] routes = {"E 2","W 2","W 1"};
         test t = new test();
-        for(int x : t.solution(name, yearning, photo)) {
-            System.out.println();
+        for(int x : t.solution(park, routes)) {
+            System.out.println(x);
         }
 
     }
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<name.length; i++){
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = new int[2];
+        int x=-1, y=-1;
 
-            map.put(name[i], yearning[i]);
-        }
-        for(int i=0; i<photo.length; i++){
-            Integer sum = 0;
-            for(int j=0; j<photo[i].length; j++){
-                if(map.containsKey(photo[i][j])) sum += map.get(photo[i][j]);
+        for(int i=0; i<park.length; i++){
+            if(park[i].indexOf('S') != -1) {
+                x = park[i].indexOf('S');
+                y = i;
             }
-            answer[i] = sum;
         }
+        System.out.println(x);
+        System.out.println(y);
+
+        for(int i=0; i<routes.length; i++){
+            String[] tmp = routes[i].split(" ");
+            String direction = tmp[0];
+            int move = Integer.parseInt(tmp[1]);
+
+            if(direction.equals("E")){
+                boolean flag = true;
+                for(int j=1; j<=move; j++){
+                    if(park[0].length() <= (x+j)){
+                        flag = false;
+                        break;
+                    }
+                    else if(park[y].charAt(x+j) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) x += move;
+            }
+            else if(direction.equals("W")){
+                boolean flag = true;
+                for(int j=1; j<=move; j++){
+                    if(0 > (x-j) ){
+                        flag = false;
+                        break;
+                    }
+                    else if(park[y].charAt(x-j) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) x -= move;
+            }
+            else if(direction.equals("S")){
+                boolean flag = true;
+                for(int j=1; j<=move; j++){
+                    if(park.length <= (y+j) ){
+                        flag = false;
+                        break;
+                    }
+                    else if(park[y+j].charAt(x) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) y += move;
+            }
+            else if(direction.equals("N")){
+                boolean flag = true;
+                for(int j=1; j<=move; j++){
+                    if(0 > (y-j) ){
+                        flag = false;
+                        break;
+                    }
+                    else if(park[y-j].charAt(x) == 'X'){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) y -= move;
+            }
+        }
+
+        answer[0] = y;
+        answer[1] = x;
         return answer;
     }
 
