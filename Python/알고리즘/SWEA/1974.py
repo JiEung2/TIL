8b1ebe4fbd2ead5c7bf422b5954916a4feff18c7
JiEung2 <181757@jnu.ac.kr>
2024-02-02 17:57:15 +0900

    2024.02.02

---
+++ Python/알고리즘/SWEA/1974.py
@@ -0,0 +1,29 @@
+def check(board):
+    for i in range(len(board)):
+        ch_row = [0] * 10
+        ch_col = [0] * 10
+        for j in range(len(board)):
+            ch_row[board[i][j]] = 1
+            ch_col[board[j][i]] = 1
+
+            if i % 3 == 0 and j % 3 == 0:
+                square_check = [0] * 10
+                for x in range(i, i + 3):
+                    for y in range(j, j + 3):
+                        if square_check[board[x][y]]:
+                            return 0
+                        square_check[board[x][y]] = 1
+
+        for j in range(1, 10):
+            if ch_row[j] == 0:
+                return 0
+            if ch_col[j] == 0:
+                return 0
+    return 1
+
+T = int(input())
+
+for tc in range(T):
+    arr = [list(map(int, input().split())) for _ in range(9)]
+    result = check(arr)
+    print(f'{tc+1} {result}')
---
+++ Python/알고리즘/SWEA/9386.py
@@ -0,0 +1,23 @@
+# 연속한 1의 개수
+
+'''
+N개의 0과 1로 이루어진 수열에서 연속한 1의 개수 중 최대값을 출력하는 프로그램을 만드시오.
+'''
+
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    numbers = input()
+
+    result = 0
+    cnt = 0
+    for i in range(N):
+        if int(numbers[i]) == 1:
+            cnt += 1
+        if int(numbers[i]) == 0 or i == N-1:
+            if result < cnt:
+                result = cnt
+            cnt = 0
+
+    print(f'#{tc+1} {result}')
