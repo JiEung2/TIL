121c0971ab7347c3bcf50f75275e5e8e7984ac03
JiEung2 <181757@jnu.ac.kr>
2024-04-27 22:16:25 +0900

    2024.04.27

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,18 +1,51 @@
-import java.lang.reflect.Field;
+//import java.lang.reflect.Field;
+//
+//class MyClass {
+//    private final String name = "JiEung";
+//}
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception {
+//        // 1. 클래스 타입.class
+//        final Class<MyClass> class1 = MyClass.class;
+//        final Field declaredField = class1.getDeclaredField("name");
+//
+//        final MyClass myClass = new MyClass();
+//        final String name = (String) declaredField.get(myClass);
+//
+//        System.out.println("name = " + name);
+//    }
+//}
 
-class MyClass {
-    private final String name = "JiEung";
+class Parent {
+    int x = 100;
+    Parent() {
+        this(500);
+    }
+
+    Parent(int x) {
+        this.x = x;
+    }
+
+    int getX() {
+        return x;
+    }
+}
+
+class Child extends Parent {
+    int x = 4000;
+    Child() {
+        this(5000);
+    }
+
+    Child(int x) {
+        this.x = x;
+    }
 }
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // 1. 클래스 타입.class
-        final Class<MyClass> class1 = MyClass.class;
-        final Field declaredField = class1.getDeclaredField("name");
-
-        final MyClass myClass = new MyClass();
-        final String name = (String) declaredField.get(myClass);
-
-        System.out.println("name = " + name);
+    public static void main(String[] args) {
+        Child obj = new Child();
+        System.out.println(obj.getX());
     }
 }
---
+++ algorithm/src/SWEA1288.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA1288 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println("#" + (tc+1) +" "+ solve(n));
+        }
+    }
+
+    public static int solve(int n) {
+        int cnt = 1;
+        int target = (1 << 10) - 1;
+        int now = 0;
+        while (true) {
+            String number = Integer.toString(n * cnt);
+            for (int i = 0; i < number.length(); i++) {
+                int tmpN = number.charAt(i) - '0';
+                now = now | (1 << tmpN);
+                if (now == target) return n * cnt;
+            }
+            cnt++;
+        }
+    }
+}
